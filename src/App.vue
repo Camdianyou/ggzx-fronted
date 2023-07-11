7fb2858f6160cb270cb87d72254e98e9be8d8c74
冲刺的派大星 <66148981+Camdianyou@users.noreply.github.com>
2023-07-11 17:16:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <h1>我是App根组件</h1>
+  </div>
+</template>
+
+<script setup lang="tsx">
+const str = "我爱你祖国";
+</script>
+
+<style lang="scss">
+.box {
+  widows: 600px;
+  height: 400px;
+  background: black;
+
+  h1 {
+    color: white;
+  }
+}
+</style>
